90036d1965fd67da49f1bedaa3dbd4a9a00d3dd6
Gaurav Bhatia <gaurav.bhatia@hurix>
2018-07-24 13:37:54 +0530

    Dev 1 Commit

--- WeatherDataDownloader/Test.swift
+++ WeatherDataDownloader/Test.swift
@@ -7,12 +7,5 @@
 //
 
 import Foundation
-<<<<<<< HEAD
 //This is Dev1
-=======
-<<<<<<< HEAD
-//This is Dev2
-=======
-//This is Dev2 
->>>>>>> 2974acf4670dcf66a47d85bbb6428506bea25123
->>>>>>> c8ed6152aba31a3f5cc68d518a4b4d24771b1bbb
+
