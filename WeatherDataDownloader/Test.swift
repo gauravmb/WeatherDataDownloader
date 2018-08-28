5a0b88877906c6df482fb5f278493de6c25e0d79
Gaurav Bhatia <gaurav.bhatia@hurix>
2018-08-28 12:06:00 +0530

    1st Commit on Branch

--- WeatherDataDownloader/Test.swift
+++ WeatherDataDownloader/Test.swift
@@ -9,42 +9,4 @@
 import Foundation
 //This is Dev1Dev2
 
-// 1st Commit on Master
-
-// 2st Commit on Master
-
-//1st Commit from branch
-
-//2st Commit from branch
-
-//3st Commit from branch
-
-//4st Commit from branch
-
-//5st Commit from branch
-
-//6st Commit from branch
-
-//7st Commit from branch
-
-//8st Commit from maeter
-
-//9st Commit from branch
-
-//10st Commit from maeter
-
-//11st Commit from branch
-
-//12st Commit from maeter
-
-//13st Commit from branch
-
-//14st Commit from branch
-
-//15st Commit from branch
-
-//16st Commit from branch
-
-//17st Commit from branch
-
-//18st Commit from branch
+// 1st Commit on Branch
